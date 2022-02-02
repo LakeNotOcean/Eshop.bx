424c276042566a8ff9815ae99f5dbe19f969390d
Nevskiy Vladislav <VNevskii650@gmail.com>
2022-02-02 13:58:25 +0200

    finish Setting and MigrationManager

--- public/index.php
+++ public/index.php
@@ -5,5 +5,3 @@ require '../Autoloader.php';
 $autoloader = Autoloader::getInstance();
 
 $autoloader->addVendorNamespacePath('Up\\', __DIR__ . '/../src/');
-
-var_dump(\Up\Core\Message\Request::createFromGlobals());
