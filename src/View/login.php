712f569379b910739d34b25ac21dbfd25c921b4d
petr <pyotr.popov443@mail.ru>
2022-02-07 19:58:00 +0200

    login verstka

---
+++ src/View/login.php
@@ -0,0 +1,14 @@
+<?php
+
+?>
+
+<!--<link rel="stylesheet" href="./css/login.css">-->
+<link rel="stylesheet" href="/public/css/login.css">
+
+<div class="container">
+	<div class="header">Вход</div>
+	<form action="/" method="post" enctype="multipart/form-data" class="sign-in">
+		<input type="text" id="login" name="login" class="input" placeholder="Логин">
+		<input type="password" id="login" name="login" class="input" placeholder="Пароль">
+	</form>
+</div>
