4806f42ea75e427ea59c39c67017f05a9f6484b2
Igor Konstantinov <kotigor0@gmail.com>
2022-02-09 19:48:19 +0200

    Igor K.: save for item in DAO

--- src/DAO/ItemDAO/ItemDAOmysql.php
+++ src/DAO/ItemDAO/ItemDAOmysql.php
@@ -3,6 +3,7 @@
 namespace Up\DAO\ItemDAO;
 
 use Up\Core\Database\DefaultDatabase;
+use Up\Entity\EntityArray;
 use Up\Entity\Item;
 use Up\Entity\ItemDetail;
 use Up\Entity\ItemsImage;
@@ -62,20 +63,17 @@ class ItemDAOmysql implements ItemDAO
 					)
 				);
 			}
-			if (!$item
-				->getSpecificationCategoriesList()
-				->getEntity($row['usc_ID'])
-				->getSpecificationList()
-				->contains($row['SPEC_TYPE_ID']))
+			if (
+				!$item->getSpecificationCategoriesList()->getEntity($row['usc_ID'])->getSpecificationList()->contains(
+						$row['SPEC_TYPE_ID']
+					)
+			)
 			{
-				$item
-					->getSpecificationCategoriesList()
-					->getEntity($row['usc_ID'])
-					->getSpecificationList()
-					->addEntity(new Specification(
-						$row['SPEC_TYPE_ID'], $row['ust_NAME'], $row['ust_DISPLAY_ORDER'], $row['VALUE']
-								)
-				);
+				$item->getSpecificationCategoriesList()->getEntity($row['usc_ID'])->getSpecificationList()->addEntity(
+						new Specification(
+							$row['SPEC_TYPE_ID'], $row['ust_NAME'], $row['ust_DISPLAY_ORDER'], $row['VALUE']
+						)
+					);
 			}
 			if (!$item->getImages()->contains($row['u_ID']))
 			{
@@ -86,6 +84,7 @@ class ItemDAOmysql implements ItemDAO
 				}
 			}
 		}
+
 		return $item;
 	}
 
@@ -102,19 +101,97 @@ class ItemDAOmysql implements ItemDAO
 	private function update(ItemDetail $item): ItemDetail
 	{
 		$oldItem = $this->getItemDetailById($item->getId());
-		$oldTags = $oldItem->getTags();
-		$oldSpecs = $oldItem->getSpecificationCategoriesList();
-		$oldImages = $oldItem->getImages();
-		$toAddTags = array_filter($item->getTags(), function(ItemsTag $tag) {
-			//return !()
-		});
 
-		return new ItemDetail();
+		$oldTags = $oldItem->getTags()->getEntitiesArray();
+		$newTags = $item->getTags()->getEntitiesArray();
+
+		$oldSpecsCat = $oldItem->getSpecificationCategoriesList()->getEntitiesArray();
+		$newSpecsCat = $item->getSpecificationCategoriesList()->getEntitiesArray();
+
+		$oldSpecs = $this->getSpecsFromCategory($oldSpecsCat);
+		$newSpecs = $this->getSpecsFromCategory($newSpecsCat);
+
+		$oldImages = $oldItem->getImages()->getEntitiesArray();
+		$newImages = $item->getImages()->getEntitiesArray();
+
+		if (!empty($oldTags))
+		{
+			$this->DBConnection->query($this->getDeleteWhereAndWhereInQuery($item->getId(), array_keys($oldTags), [
+				'table_name' => 'up_item_tag',
+				'item_id_name' => 'ITEM_ID',
+				'other_id_name' => 'TAG_ID'])
+			);
+		}
+		if(!empty($oldSpecs))
+		{
+			$this->DBConnection->query($this->getDeleteWhereAndWhereInQuery($item->getId(), array_keys($oldSpecs), [
+				'table_name' => 'up_item_spec',
+				'item_id_name' => 'ITEM_ID',
+				'other_id_name' => 'SPEC_TYPE_ID'])
+			);
+		}
+		if(!empty($oldImages))
+		{
+			$this->DBConnection->query($this->getDeleteWhereAndWhereInQuery($item->getId(), array_keys($oldImages),[
+				'table_name' => 'up_image',
+				'item_id_name' => 'ITEM_ID',
+				'other_id_name' => 'ID'])
+			);
+		}
+		if(!empty($newTags))
+		{
+			$this->DBConnection->query($this->getInsertTagsQuery($item->getId(), $newTags));
+		}
+		if(!empty($newSpecs))
+		{
+			$this->DBConnection->query($this->getInsertSpecsQuery($item->getId(), $newSpecs));
+		}
+		if(!empty($newImages))
+		{
+			$this->DBConnection->query($this->getInsertImagesQuery($item->getId(), $newImages));
+		}
+		$this->DBConnection->query($this->getUpdateItemQuery($item));
+
+		return $item;
 	}
 
-	private function create(ItemDetail $itemDetail): ItemDetail
+	private function create(ItemDetail $item): ItemDetail
 	{
-		return new ItemDetail();
+		$this->DBConnection->query($this->getInsertItemQuery($item));
+		$id = $this->DBConnection->lastInsertId();
+		$item->setId($id);
+		$tags = $item->getTags()->getEntitiesArray();
+
+		$specsCat = $item->getSpecificationCategoriesList()->getEntitiesArray();
+
+		$specs = $this->getSpecsFromCategory($specsCat);
+
+		$images = $item->getImages()->getEntitiesArray();
+
+		$this->DBConnection->query($this->getInsertTagsQuery($id, $tags));
+		$this->DBConnection->query($this->getInsertSpecsQuery($id, $specs));
+		$this->DBConnection->query($this->getInsertImagesQuery($id, $images));
+		return $item;
+	}
+
+	/**
+	 * @param array<int,SpecificationCategory> $categories
+	 *
+	 * @return array
+	 */
+	private function getSpecsFromCategory(array $categories): array
+	{
+		$specs = [];
+		foreach ($categories as $cat)
+		{
+			$category = $cat->getSpecificationList()->getEntitiesArray();
+			foreach ($cat as $id => $spec)
+			{
+				$specs[$id] = $spec;
+			}
+		}
+
+		return $specs;
 	}
 
 	private function getItemsQuery(int $from, int $to): string
@@ -156,6 +233,64 @@ class ItemDAOmysql implements ItemDAO
                 INNER JOIN up_spec_type ust on uis.SPEC_TYPE_ID = ust.ID
                 INNER JOIN up_spec_category usc on ust.SPEC_CATEGORY_ID = usc.ID
                 INNER JOIN up_image u on ui.ID = u.ITEM_ID;";
+	}
+
+	private function getDeleteWhereAndWhereInQuery(int $id, array $ids, array $table): string
+	{
+		$whereIn = '(' . implode(',', $ids) . ')';
+
+		return "DELETE FROM {$table['table_name']} WHERE {$table['item_id_name']}={$id} AND {$table['other_id_name']} IN {$whereIn};";
+	}
+
+	private function getInsertTagsQuery(int $id, array $tags): string
+	{
+		$insert = implode(
+			',',
+			array_map(function(ItemsTag $tag) use ($id) {
+				return "({$id},{$tag->getId()})";
+			}, $tags)
+		);
+		return "INSERT INTO up_item_tag (ITEM_ID, TAG_ID) VALUES {$insert};";
+	}
+
+	private function getInsertSpecsQuery(int $id, array $specs): string
+	{
+		$insert = implode(',', array_map(function(Specification $s) use ($id){
+			return "({$id},{$s->getId()},{$s->getValue()})";
+		}, $specs));
+		return "INSERT INTO up_item_spec (ITEM_ID, SPEC_TYPE_ID, VALUE) VALUES {$insert};";
+	}
+
+	private function getInsertImagesQuery(int $id, array $images): string
+	{
+		$insert = implode(',', array_map(function(ItemsImage $image) use($id){
+			return "({$image->getPath()},{$id},{$image->isMain()})";
+		}, $images));
+		return "INSERT INTO up_image(PATH, ITEM_ID, IS_MAIN) VALUES {$insert};";
+	}
+
+	private function getInsertItemQuery(ItemDetail $item): string
+	{
+		$date = date('Y-m-d H:i:s');
+		return "INSERT INTO up_item(TITLE, PRICE, SHORT_DESC, FULL_DESC, SORT_ORDER, ACTIVE, DATE_CREATE, DATE_UPDATE, ITEM_TYPE_ID) 
+				VALUES ({$item->getTitle()}, {$item->getPrice()}, {$item->getShortDescription()}, 
+				        {$item->getFullDescription()}, {$item->getSortOrder()}, {$item->getIsActive()}, 
+				        {$date}, {$date}, {$item->getItemType()->getId()});";
+	}
+
+	private function getUpdateItemQuery(ItemDetail $item): string
+	{
+		$date = date('Y-m-d H:i:s');
+		return "UPDATE up_item
+				SET TITLE={$item->getTitle()},
+					PRICE={$item->getPrice()},
+					SHORT_DESC={$item->getShortDescription()},
+					FULL_DESC={$item->getFullDescription()},
+					SORT_ORDER={$item->getSortOrder()},
+					ACTIVE={$item->getIsActive()},
+				    ITEM_TYPE_ID={$item->getItemType()->getId()},
+				    DATE_UPDATE={$date}
+				WHERE ID={$item->getId()};";
 	}
 
 	private function mapItemCommonInfo(Item $item, array $row)
