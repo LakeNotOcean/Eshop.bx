0abf664ab033e8e3bf4594672ad41b7585ccd175
petr <pyotr.popov443@mail.ru>
2022-02-12 14:22:27 +0200

    index returned to normal

--- public/index.php
+++ public/index.php
@@ -2,8 +2,6 @@
 
 declare(strict_types=1);
 
-$start = microtime(true); // Top of page
-
 require '../Autoloader.php';
 
 
@@ -14,13 +12,3 @@ $autoloader->addVendorNamespacePath('Up\\', __DIR__ . '/../src/');
 session_start();
 
 Up\Core\Application::run();
-
-$end = microtime(true); // Bottom of page
-
-$time = $end - $start;
-$content ="Page loaded in $time seconds";
-?>
-
-<div style="position: absolute; z-index: 999; top: 0; left: 0">
-	<?= $content?>
-</div>
