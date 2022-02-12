27d9fd1ec960a8e6bb2474b42138923dda95a353
petr <pyotr.popov443@mail.ru>
2022-02-12 14:22:27 +0200

    index returned to normal

--- public/index.php
+++ public/index.php
@@ -3,8 +3,6 @@
 declare(strict_types=1);
 const SOURCE_DIR = __DIR__ . '/../src';
 
-$start = microtime(true); // Top of page
-
 require '../Autoloader.php';
 
 
@@ -15,13 +13,3 @@ $autoloader->addVendorNamespacePath('Up\\', __DIR__ . '/../src/');
 session_start();
 
 Up\Core\Application::run();
-
-$end = microtime(true); // Bottom of page
-
-$time = $end - $start;
-$content ="Page loaded in $time seconds";
-?>
-
-<div style="position: absolute; z-index: 999; top: 0; left: 0">
-	<?= $content?>
-</div>
