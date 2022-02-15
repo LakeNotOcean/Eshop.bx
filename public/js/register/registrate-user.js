a5c29908f8ad33e0f7452f19c3693f0bf1b4a5ca
Nevskiy Vladislav <VNevskii650@gmail.com>
2022-02-15 15:35:35 +0200

    Закончена верстка страницы регистрации

---
+++ public/js/register/registrate-user.js
@@ -0,0 +1,8 @@
+const submitButton=document.getElementById('submit');
+const loginValue=document.getElementById('login');
+const firstName=document.getElementById('firstName');
+const secondName=document.getElementById('secondName');
+const passwordValue=document.getElementById('password');
+const repeatPasswordValue=document.getElementById('repeatPassword');
+const email=document.getElementById('email');
+const phone=document.getElementById('phone');
--- src/View/register.php
+++ src/View/register.php
@@ -6,12 +6,83 @@
 <link rel="stylesheet" href="/css/login.css">
 
 <div class="container">
-	<div class="sign-in-title">Вход</div>
-	<form action="/register" method="post" enctype="multipart/form-data" class="sign-in-form">
-		<input type="text" id="login" name="login" class="input" placeholder="Логин">
-		<input type="password" id="password" name="password" class="input" placeholder="Пароль">
-		<input type="text" id="email" name="email" class="input" placeholder="Почта">
-		<input type="text" id="phone" name="phone" class="input" placeholder="Телефон">
-		<input type="submit" id="submit" class="button" value="Отрпавить">
-	</form>
+	<div class="sign-in-title">Регистрация</div>
+	<div class="register-fields">
+		<label for="login" class="field">
+			<div class="label-input">
+				<span class="label-title">Полное имя</span>
+				<div class="input-error">
+					<input type="text" class="input" id="firstName" name="firstName" placeholder="Полное имя">
+					<span class="label-error">Ошибка</span>
+				</div>
+			</div>
+		</label>
+		<label for="login" class="field">
+			<div class="label-input">
+				<span class="label-title">Фамилия</span>
+				<div class="input-error">
+					<input type="text" class="input" id="SecondName" name="SecondName" placeholder="Фамилия">
+					<span class="label-error">Ошибка</span>
+				</div>
+			</div>
+		<label for="login" class="field">
+			<div class="label-input">
+				<span class="label-title">Логин</span>
+				<div class="input-error">
+					<input type="text" class="input" id="login" name="login" placeholder="Логин">
+					<span class="label-error">Ошибка</span>
+				</div>
+			</div>
+		</label>
+
+
+
+		</label>
+		<label for="login" class="field">
+			<div class="label-input">
+				<span class="label-title">Пароль</span>
+				<div class="input-error">
+					<input type="password" class="input" id="password" name="password" placeholder="Пароль">
+					<span class="label-error">Ошибка</span>
+				</div>
+			</div>
+		</label>
+		<label for="login" class="field">
+			<div class="label-input">
+				<span class="label-title">Повторите Пароль</span>
+				<div class="input-error">
+					<input type="password" class="input" id="repeatPassword" name="repeatPassword" placeholder="Повторите пароль">
+					<span class="label-error">Ошибка</span>
+				</div>
+			</div>
+			<label for="login" class="field">
+				<div class="label-input">
+					<span class="label-title">Email</span>
+					<div class="input-error">
+						<input type="text" class="input" id="email" name="email" placeholder="Email">
+						<span class="label-error">Ошибка</span>
+					</div>
+				</div>
+			</label>
+		</label>
+		<label for="login" class="field">
+			<div class="label-input">
+				<span class="label-title">Контактный телефон</span>
+				<div class="input-error">
+					<input type="text" class="input" id="phone" name="phone" placeholder="Контактный телефон">
+					<span class="label-error">Ошибка</span>
+				</div>
+			</div>
+		</label>
+		<input type="submit" id="submit" value="Зарегестрироваться" class="btn btn-save">
+	</div>
+<!--	<form action="/register" method="post" enctype="multipart/form-data" class="sign-in-form">-->
+<!--		<input type="text" id="login" name="login" class="input" placeholder="Логин">-->
+<!--		<input type="password" id="password" name="password" class="input" placeholder="Пароль">-->
+<!--		<input type="text" id="email" name="email" class="input" placeholder="Почта">-->
+<!--		<input type="text" id="phone" name="phone" class="input" placeholder="Телефон">-->
+<!--		<input type="submit" id="submit" class="button" value="Отрпавить">-->
+<!--		<input type="text">-->
+<!--	</form>-->
+
 </div>
