0ac5f9fa5fd9b56a091975671e2744e361026f5a
Kalyugin Oleg <oleg.kalyugin2001@mail.ru>
2022-02-15 16:09:59 +0200

    Убрал алерты

--- public/js/queryPush.js
+++ public/js/queryPush.js
@@ -4,13 +4,10 @@ for (let pageButton of pageButtons)
 {
 	pageButton.addEventListener('click',(e) => {
 		let value = e.target.id;
-		alert(value)
 		let url = new URL(window.location);
 		url.searchParams.set("page", value);
 		url = url.search;
 		localStorage.setItem('query', url)
-
-		alert(url.toString())
 		location = url
 	});
 }
