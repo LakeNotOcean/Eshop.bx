a71f1bbc228a377903d4ea9a7e08b19ede3cb809
Kalyugin Oleg <oleg.kalyugin2001@mail.ru>
2022-02-17 22:48:19 +0200

    Миграция к шаблонам

---
+++ src/Core/MigrationScripts/2022_02_17_22-19-07_test_spec_template.sql
@@ -0,0 +1,54 @@
+INSERT INTO eshop.up_spec_template (ITEM_TYPE_ID, SPEC_TYPE_ID)
+VALUES (1, 1);
+
+INSERT INTO eshop.up_spec_template (ITEM_TYPE_ID, SPEC_TYPE_ID)
+VALUES (1, 2);
+
+INSERT INTO eshop.up_spec_template (ITEM_TYPE_ID, SPEC_TYPE_ID)
+VALUES (1, 3);
+
+INSERT INTO eshop.up_spec_template (ITEM_TYPE_ID, SPEC_TYPE_ID)
+VALUES (1, 4);
+
+INSERT INTO eshop.up_spec_template (ITEM_TYPE_ID, SPEC_TYPE_ID)
+VALUES (1, 5);
+
+INSERT INTO eshop.up_spec_template (ITEM_TYPE_ID, SPEC_TYPE_ID)
+VALUES (1, 6);
+
+INSERT INTO eshop.up_spec_template (ITEM_TYPE_ID, SPEC_TYPE_ID)
+VALUES (1, 7);
+
+INSERT INTO eshop.up_spec_template (ITEM_TYPE_ID, SPEC_TYPE_ID)
+VALUES (1, 8);
+
+INSERT INTO eshop.up_spec_template (ITEM_TYPE_ID, SPEC_TYPE_ID)
+VALUES (1, 9);
+
+INSERT INTO eshop.up_spec_template (ITEM_TYPE_ID, SPEC_TYPE_ID)
+VALUES (1, 10);
+
+INSERT INTO eshop.up_spec_template (ITEM_TYPE_ID, SPEC_TYPE_ID)
+VALUES (1, 11);
+
+INSERT INTO eshop.up_spec_template (ITEM_TYPE_ID, SPEC_TYPE_ID)
+VALUES (1, 12);
+
+INSERT INTO eshop.up_spec_template (ITEM_TYPE_ID, SPEC_TYPE_ID)
+VALUES (1, 13);
+
+INSERT INTO eshop.up_spec_template (ITEM_TYPE_ID, SPEC_TYPE_ID)
+VALUES (1, 14);
+
+INSERT INTO eshop.up_spec_template (ITEM_TYPE_ID, SPEC_TYPE_ID)
+VALUES (1, 15);
+
+INSERT INTO eshop.up_spec_template (ITEM_TYPE_ID, SPEC_TYPE_ID)
+VALUES (1, 16);
+
+INSERT INTO eshop.up_spec_template (ITEM_TYPE_ID, SPEC_TYPE_ID)
+VALUES (1, 17);
+
+INSERT INTO eshop.up_spec_template (ITEM_TYPE_ID, SPEC_TYPE_ID)
+VALUES (1, 18);
+
