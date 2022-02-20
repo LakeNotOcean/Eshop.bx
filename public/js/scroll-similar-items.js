3c541b9e602739290251845b2f65d628dd7e7219
Kalyugin Oleg <oleg.kalyugin2001@mail.ru>
2022-02-20 22:15:04 +0200

    Изменение логики переключения

--- public/js/scroll-similar-items.js
+++ public/js/scroll-similar-items.js
@@ -1,17 +1,25 @@
 let scrollablePlace = document.querySelector('.similar-item-cards-section')
-let similarItem = document.querySelector(".similar-item-card")
-let similarItemWidth = similarItem.offsetWidth
+let similarItems = document.querySelectorAll(".similar-item-card")
+let similarItemWidth = similarItems[0].offsetWidth
 let leftSimilarItemScroll = document.querySelector(".similar-item-cards-left-arrow");
 let rightSimilarItemScroll = document.querySelector(".similar-item-cards-right-arrow");
 
 rightSimilarItemScroll.addEventListener('click', (e) => {
 	console.log(scrollablePlace.scrollLeft)
-	scrollablePlace.scroll(scrollablePlace.scrollLeft + similarItemWidth + 22,0)
+	console.log(similarItemWidth * (similarItems.length + 2))
+	if (scrollablePlace.scrollLeft <= similarItemWidth * (similarItems.length - 2))
+	{
+		scrollablePlace.scroll(scrollablePlace.scrollLeft + similarItemWidth + 20,0)
+	}
+
 })
 
 
 leftSimilarItemScroll.addEventListener('click', (e) => {
-	scrollablePlace.scroll(scrollablePlace.scrollLeft - similarItemWidth - 22,0)
+
+		scrollablePlace.scroll(scrollablePlace.scrollLeft - similarItemWidth - 20,0)
+
+
 })
 
 
