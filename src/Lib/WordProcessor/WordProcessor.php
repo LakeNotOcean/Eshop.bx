b21f9b5887459586c1866717be8545a9de14a90e
Artyom Rogov <artyomrogov2012@gmail.com>
2022-02-23 21:38:59 +0200

    WordProcessor: добавлен метод для создания формы слова по числительному рядом с ним (1 товар, 2 товара)

---
+++ src/Lib/WordProcessor/WordProcessor.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Up\Lib\Wordprocessor;
+
+class WordProcessor
+{
+	public static function formatWord(int $amount, string $word): string
+	{
+		if (in_array($amount % 10, [0, 5, 6, 7, 8, 9]) || in_array($amount % 100, [11, 12, 13, 14]))
+		{
+			return $word . 'ов';
+		}
+
+		if ($amount % 10 === 1)
+		{
+			return $word;
+		}
+
+		return $word . 'a';
+	}
+}
