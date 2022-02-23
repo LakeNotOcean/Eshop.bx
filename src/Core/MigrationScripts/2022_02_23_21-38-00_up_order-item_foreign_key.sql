8608f012c7c22521c73977f88a9600a64d3726c5
Igor Konstantinov <kotigor0@gmail.com>
2022-02-24 01:07:49 +0200

    Igor K.: reviews + migration

--- src/Core/MigrationScripts/2022_02_23_21-38-00_up_order-item_foreign_key.sql
+++ src/Core/MigrationScripts/2022_02_23_21-38-00_up_order-item_foreign_key.sql
@@ -1,5 +1,3 @@
-alter table `up_order-item` drop foreign key `up_order-item_up_order_ID_fk`;
-
 alter table `up_order-item`
 	add constraint `up_order-item_up_order_ID_fk`
 		foreign key (ORDER_ID) references up_order (ID)
--- src/Core/MigrationScripts/2022_02_23_21-39-00_add_foreign_and_primary_key_in_some_table.sql
+++ src/Core/MigrationScripts/2022_02_23_21-39-00_add_foreign_and_primary_key_in_some_table.sql
@@ -2,10 +2,6 @@ alter table `up_order-item`
 	add constraint `up_order-item_pk`
 		primary key (ORDER_ID, ITEM_ID);
 
-alter table `up_order-item`
-	add constraint `up_order-item_up_order_ID_fk`
-		foreign key (ORDER_ID) references up_order (ID);
-
 alter table up_review
 	add constraint up_review_pk
 		primary key (ID);
