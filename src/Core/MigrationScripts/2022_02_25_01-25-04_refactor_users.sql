9c6f39ec354a4b5abe3a02ef558d57ddb38ba6d7
Kalyugin Oleg <oleg.kalyugin2001@mail.ru>
2022-02-25 01:25:47 +0200

    Изменил имена пользователей

---
+++ src/Core/MigrationScripts/2022_02_25_01-25-04_refactor_users.sql
@@ -0,0 +1,48 @@
+UPDATE eshop.up_user t
+SET t.LOGIN = 'admin5'
+WHERE t.ID = 5;
+
+UPDATE eshop.up_user t
+SET t.LOGIN = 'admin11'
+WHERE t.ID = 11;
+
+UPDATE eshop.up_user t
+SET t.LOGIN = 'admin12'
+WHERE t.ID = 12;
+
+UPDATE eshop.up_user t
+SET t.LOGIN = 'admin10'
+WHERE t.ID = 10;
+
+UPDATE eshop.up_user t
+SET t.LOGIN = 'admin8'
+WHERE t.ID = 8;
+
+UPDATE eshop.up_user t
+SET t.LOGIN = 'admin6'
+WHERE t.ID = 6;
+
+UPDATE eshop.up_user t
+SET t.LOGIN = 'admin7'
+WHERE t.ID = 7;
+
+UPDATE eshop.up_user t
+SET t.LOGIN = 'admin13'
+WHERE t.ID = 13;
+
+UPDATE eshop.up_user t
+SET t.LOGIN = 'admin3'
+WHERE t.ID = 3;
+
+UPDATE eshop.up_user t
+SET t.LOGIN = 'admin14'
+WHERE t.ID = 14;
+
+UPDATE eshop.up_user t
+SET t.LOGIN = 'admin4'
+WHERE t.ID = 4;
+
+UPDATE eshop.up_user t
+SET t.LOGIN = 'admin9'
+WHERE t.ID = 9;
+
