31e753b5b366b804345703fb20ffb6fa69677c4f
petr <pyotr.popov443@mail.ru>
2022-02-25 17:08:09 +0200

    more reviews

--- src/View/more-reviews.php
+++ src/View/more-reviews.php
@@ -88,7 +88,7 @@ use Up\Lib\FormatHelper\WordEndingResolver;
 			<?php if($review->getUser()->getId() == $user->getId()): ?>
 				<div class="your-review-msg">Это ваш отзыв</div>
 			<?php endif; ?>
-			<?php if($review->getUser()->getId() == $user->getId() || $user->getRole()->getName() === \Up\Entity\User\UserEnum::Admin()): ?>
+			<?php if($review->getUser()->getId() == $user->getId() || $user->getRole()->getName() == \Up\Entity\User\UserEnum::Admin()): ?>
 			<div class="review-remove-btn"></div>
 			<input type="hidden" name="review_id" value="<?= $review->getId() ?>">
 			<?php endif; ?>
