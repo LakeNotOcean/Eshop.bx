859a91e468260a7a40166e8b35c1855545f6d85e
Igor Konstantinov <kotigor0@gmail.com>
2022-03-03 14:33:23 +0200

    Igor K.: fix migration(удалены цифры из имен и фамилий тестовых юзеров)

--- src/Core/MigrationScripts/2022_02_24_03-15-28_add_test_users.sql
+++ src/Core/MigrationScripts/2022_02_24_03-15-28_add_test_users.sql
@@ -1,51 +1,51 @@
 INSERT INTO eshop.up_user (ID, LOGIN, PASSWORD, ROLE_ID, EMAIL, PHONE, FIRST_NAME, SECOND_NAME)
-VALUES (2, 'admin2', '$2y$10$gfQtxmPJ89kVwkSMqB2GHOgtDQtNT4wpoW8yEaJTQNpX.ncs5vc2C', 2, 'cal.kal2@mail.ru',
-        '+7 (967) 268-76-86', 'admin2', 'admin2');
+VALUES (2, 'admin2', '$2y$10$gfQtxmPJ89kVwkSMqB2GHOgtDQtNT4wpoW8yEaJTQNpX.ncs5vc2C', 1, 'cal.kal2@mail.ru',
+        '+7 (967) 268-76-86', 'admin', 'admin');
 
 INSERT INTO eshop.up_user (ID, LOGIN, PASSWORD, ROLE_ID, EMAIL, PHONE, FIRST_NAME, SECOND_NAME)
 VALUES (3, 'admin21', '$2y$10$gfQtxmPJ89kVwkSMqB2GHOgtDQtNT4wpoW8yEaJTQNpX.ncs5vc2C', 2, 'cal.kal122@mail.ru',
-        '+7 (967) 868-86-86', 'admin2', 'admin2');
+        '+7 (967) 868-86-86', 'admin', 'admin');
 
 INSERT INTO eshop.up_user (ID, LOGIN, PASSWORD, ROLE_ID, EMAIL, PHONE, FIRST_NAME, SECOND_NAME)
 VALUES (4, 'admin32', '$2y$10$gfQtxmPJ89kVwkSMqB2GHOgtDQtNT4wpoW8yEaJTQNpX.ncs5vc2C', 2, 'cal.kal42@mail.ru',
-        '+7 (967) 868-66-86', 'admin2', 'admin2');
+        '+7 (967) 868-66-86', 'admin', 'admin');
 
 INSERT INTO eshop.up_user (ID, LOGIN, PASSWORD, ROLE_ID, EMAIL, PHONE, FIRST_NAME, SECOND_NAME)
 VALUES (5, 'adm3in2', '$2y$10$gfQtxmPJ89kVwkSMqB2GHOgtDQtNT4wpoW8yEaJTQNpX.ncs5vc2C', 2, 'cal.kald2@mail.ru',
-        '+7 (967) 868-75-86', 'admin2', 'admin2');
+        '+7 (967) 868-75-86', 'admin', 'admin');
 
 INSERT INTO eshop.up_user (ID, LOGIN, PASSWORD, ROLE_ID, EMAIL, PHONE, FIRST_NAME, SECOND_NAME)
 VALUES (6, 'adm2in2', '$2y$10$gfQtxmPJ89kVwkSMqB2GHOgtDQtNT4wpoW8yEaJTQNpX.ncs5vc2C', 2, 'cal.kalg2@mail.ru',
-        '+7 (967) 868-36-86', 'admin2', 'admin2');
+        '+7 (967) 868-36-86', 'admin', 'admin');
 
 INSERT INTO eshop.up_user (ID, LOGIN, PASSWORD, ROLE_ID, EMAIL, PHONE, FIRST_NAME, SECOND_NAME)
 VALUES (7, 'adm4in2', '$2y$10$gfQtxmPJ89kVwkSMqB2GHOgtDQtNT4wpoW8yEaJTQNpX.ncs5vc2C', 2, 'cal.khal2@mail.ru',
-        '+7 (967) 868-72-86', 'admin2', 'admin2');
+        '+7 (967) 868-72-86', 'admin', 'admin');
 
 INSERT INTO eshop.up_user (ID, LOGIN, PASSWORD, ROLE_ID, EMAIL, PHONE, FIRST_NAME, SECOND_NAME)
 VALUES (8, 'ad5min2', '$2y$10$gfQtxmPJ89kVwkSMqB2GHOgtDQtNT4wpoW8yEaJTQNpX.ncs5vc2C', 2, 'cal.kgal2@mail.ru',
-        '+7 (967) 869-76-86', 'admin2', 'admin2');
+        '+7 (967) 869-76-86', 'admin', 'admin');
 
 INSERT INTO eshop.up_user (ID, LOGIN, PASSWORD, ROLE_ID, EMAIL, PHONE, FIRST_NAME, SECOND_NAME)
 VALUES (9, 'ad6min2', '$2y$10$gfQtxmPJ89kVwkSMqB2GHOgtDQtNT4wpoW8yEaJTQNpX.ncs5vc2C', 2, 'calj.kal2@mail.ru',
-        '+7 (967) 867-76-86', 'admin2', 'admin2');
+        '+7 (967) 867-76-86', 'admin', 'admin');
 
 INSERT INTO eshop.up_user (ID, LOGIN, PASSWORD, ROLE_ID, EMAIL, PHONE, FIRST_NAME, SECOND_NAME)
 VALUES (10, 'adm7in2', '$2y$10$gfQtxmPJ89kVwkSMqB2GHOgtDQtNT4wpoW8yEaJTQNpX.ncs5vc2C', 2, 'calh.kal2@mail.ru',
-        '+7 (967) 866-76-86', 'admin2', 'admin2');
+        '+7 (967) 866-76-86', 'admin', 'admin');
 
 INSERT INTO eshop.up_user (ID, LOGIN, PASSWORD, ROLE_ID, EMAIL, PHONE, FIRST_NAME, SECOND_NAME)
 VALUES (11, 'ad8min2', '$2y$10$gfQtxmPJ89kVwkSMqB2GHOgtDQtNT4wpoW8yEaJTQNpX.ncs5vc2C', 2, 'cal.gkal2@mail.ru',
-        '+7 (967) 865-76-86', 'admin2', 'admin2');
+        '+7 (967) 865-76-86', 'admin', 'admin');
 
 INSERT INTO eshop.up_user (ID, LOGIN, PASSWORD, ROLE_ID, EMAIL, PHONE, FIRST_NAME, SECOND_NAME)
 VALUES (12, 'ad9min2', '$2y$10$gfQtxmPJ89kVwkSMqB2GHOgtDQtNT4wpoW8yEaJTQNpX.ncs5vc2C', 2, 'calk.kal2@mail.ru',
-        '+7 (967) 848-76-86', 'admin2', 'admin2');
+        '+7 (967) 848-76-86', 'admin', 'admin');
 
 INSERT INTO eshop.up_user (ID, LOGIN, PASSWORD, ROLE_ID, EMAIL, PHONE, FIRST_NAME, SECOND_NAME)
 VALUES (13, 'ad34min2', '$2y$10$gfQtxmPJ89kVwkSMqB2GHOgtDQtNT4wpoW8yEaJTQNpX.ncs5vc2C', 2, 'cavl.kal2@mail.ru',
-        '+7 (967) 838-76-86', 'admin2', 'admin2');
+        '+7 (967) 838-76-86', 'admin', 'admin');
 
 INSERT INTO eshop.up_user (ID, LOGIN, PASSWORD, ROLE_ID, EMAIL, PHONE, FIRST_NAME, SECOND_NAME)
 VALUES (14, 'ad23min2', '$2y$10$gfQtxmPJ89kVwkSMqB2GHOgtDQtNT4wpoW8yEaJTQNpX.ncs5vc2C', 2, 'cal.ckal2@mail.ru',
-        '+7 (967) 868-26-86', 'admin2', 'admin2');
+        '+7 (967) 868-26-86', 'admin', 'admin');
